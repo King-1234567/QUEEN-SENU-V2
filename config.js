c5fa1292c133f1f032338aee9aadb3a18dea3a2b
King-1234567 <kingchanu1234@gmail.com>
2025-02-18 07:15:00 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,6 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || 'KIgknJqS#ta_8waLqM5V35eIYeX1Cl7xk4PA0fRaJSQ5BjYZVCDk',
+    SESSION_ID: process.env.SESSION_ID || 'OIQyCJqZ#Z8SImtsk-s1yNTbJIfyTJGg_3vh9g0aKxgpYn4D82PU',
     MONGODB: process.env.MONGODB || 'mongodb+srv://udavin56:1234@cluster0.urhma.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
 };
